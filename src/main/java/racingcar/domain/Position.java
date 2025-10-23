2865a1111c35a6b224f00c19f53766e5ec7c00d3
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-10-23 15:02:26 +0900

    feat(domain): add Position value object with validation logic

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,4 +1,24 @@
 package racingcar.domain;
 
 public class Position {
+    private final int value;
+
+    public Position(int value) {
+        validateNonNegative(value);
+        this.value = value;
+    }
+
+    public Position forward() {
+        return new Position(value + 1);
+    }
+
+    public int value() {
+        return value;
+    }
+
+    private static void validateNonNegative(int value) {
+        if(value < 0){
+            throw new IllegalArgumentException("위치는 음수가 될 수 없습니다.");
+        }
+    }
 }
