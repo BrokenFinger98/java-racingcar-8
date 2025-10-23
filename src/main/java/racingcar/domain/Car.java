ed9037cc5ab635b43441d2b55699c117d8e70099
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-10-23 15:12:34 +0900

    feat(domain): add Car entity with name and position, supporting conditional movement

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,24 @@
 package racingcar.domain;
 
 public class Car {
+    private final CarName name;
+    private Position position = new Position(0);
+
+    public Car(CarName name) {
+        this.name = name;
+    }
+
+    public void moveIf(MoveStrategy strategy) {
+        if (strategy.shouldMove()) {
+            position = position.forward();
+        }
+    }
+
+    public String name() {
+        return name.value();
+    }
+
+    public int position() {
+        return position.value();
+    }
 }
