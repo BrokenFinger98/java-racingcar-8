f079b79e6bb07c4e2659ea114e875aa2794e9897
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-10-23 13:19:19 +0900

    refactor(domain): simplify random number generation in RandomMoveStrategy

--- src/main/java/racingcar/domain/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -1,14 +1,11 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Arrays;
-import java.util.List;
 
 public class RandomMoveStrategy implements MoveStrategy {
-    private static final List<Integer> NUMBERS = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
     @Override
     public boolean shouldMove() {
-        return Randoms.pickNumberInList(NUMBERS) >= 4;
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
